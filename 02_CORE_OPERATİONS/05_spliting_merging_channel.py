44acc450819480b4808b6cf926784332f55b4d28
enescankaya <enescankaya6844100@gmail.com>
2024-08-14 16:41:57 +0300

    updates

--- 02_CORE_OPERATİONS/05_spliting_merging_channel.py
+++ 02_CORE_OPERATİONS/05_spliting_merging_channel.py
@@ -2,22 +2,52 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-path="TEST_IMAGES/opencv_logo.png"
-img=cv2.imread(path)
-print(  "Shape: {}".format(img.shape))
-(B,G,R)=cv2.split(img)#cv2.split komutu channeldaki 3 rengi çıkarıp yeni resme atayabilmemizi sağlar. burada b,g,r adlı yeni değişkenlere sırasıyla maviyi,yeşili ve kırmızı
-#rengi çıkarıp onlara atadık
-#bu renkleri tekrar birleştirip orjinal görüntü elde edebiliriz. aşşağıdaki gibi merge komutu kullanarak
-merged=cv2.merge([B,G,R])
-black=np.zeros(img.shape[:2],dtype="uint8")#siyah pencere oluşturur/görüntüyü siyaha çevirir
-cv2.imshow("black",black)
-cv2.imshow("Red",cv2.merge([black,black,R]))
-cv2.imshow("Green",cv2.merge([black,G,black]))
-cv2.imshow("Blue",cv2.merge([B,black,black]))
-# cv2.imshow("MERGED",merged)
-cv2.imshow("image",img)
-# cv2.imshow("B",B)
-# cv2.imshow("G",G)
-# cv2.imshow("R",R)
+# Görüntü dosyasının yolunu belirtiyoruz
+path = "TEST_IMAGES/opencv_logo.png"
+
+# cv2.imread() fonksiyonu ile resmi yüklüyoruz
+img = cv2.imread(path)
+
+# img.shape komutuyla resmin boyutlarını yazdırıyoruz (yükseklik, genişlik, kanal sayısı)
+print("Shape: {}".format(img.shape))
+
+# cv2.split() fonksiyonu ile görüntüyü üç ayrı kanala (B, G, R) bölüyoruz
+# Bu, görüntünün mavi, yeşil ve kırmızı bileşenlerini ayrı ayrı elde etmemizi sağlar
+(B, G, R) = cv2.split(img)
+
+# cv2.merge() fonksiyonu ile üç renk kanalını birleştirip orijinal görüntüyü yeniden oluşturuyoruz
+merged = cv2.merge([B, G, R])
+
+# RGB olarak, BGR kanallarını sıralama şekli değiştiriyoruz (BGR'den RGB'ye dönüştürüyoruz)
+RGB = cv2.merge([R, G, B])
+
+# Siyah bir görüntü oluşturuyoruz, boyut olarak orijinal resmin yüksekliği ve genişliğiyle aynı
+# dtype="uint8" kullanarak veri tipini belirliyoruz
+black = np.zeros(img.shape[:2], dtype="uint8")
+
+# Oluşturduğumuz siyah görüntüyü gösteriyoruz
+cv2.imshow("black", black)
+
+# Kırmızı kanal görüntüsü, diğer iki kanal siyah olacak şekilde birleştiriyoruz
+cv2.imshow("Red", cv2.merge([black, black, R]))
+
+# Yeşil kanal görüntüsü, diğer iki kanal siyah olacak şekilde birleştiriyoruz
+cv2.imshow("Green", cv2.merge([black, G, black]))
+
+# Mavi kanal görüntüsü, diğer iki kanal siyah olacak şekilde birleştiriyoruz
+cv2.imshow("Blue", cv2.merge([B, black, black]))
+
+# Orijinal görüntünün yeniden oluşturulmuş halini gösteriyoruz
+cv2.imshow("MERGED", merged)
+
+# Orijinal görüntüyü gösteriyoruz
+cv2.imshow("image", img)
+
+# BGR'den RGB'ye dönüştürülmüş görüntüyü gösteriyoruz
+cv2.imshow("RGB", RGB)
+
+# Bir tuşa basılmasını bekliyoruz, böylece pencereler kapanmaz
 cv2.waitKey(0)
+
+# Tüm açık pencereleri kapatıyoruz
 cv2.destroyAllWindows()
